f7ddb45cbb94ba455308b4ffcb2dc9bb2dddaa13
olaogun <tolustar29@gmail.com>
2022-09-22 09:35:16 +0100

    Updated package.json

--- packages/product-service/server/index.js
+++ packages/product-service/server/index.js
@@ -1,9 +1,8 @@
 const app = require('./app');
-const debug = require('debug')('example:server');
 
 const port = parseInt(process.env.PORT || '3001', 10);
 app.set('port', port);
 
 app.listen(app.get('port'), function () {
-  debug(`Product server listening on port ${app.get('port')}`);
+  console.log(`Product server listening on port ${app.get('port')}`);
 });
--- packages/user-service/server/index.js
+++ packages/user-service/server/index.js
@@ -1,9 +1,8 @@
 const app = require('./app');
-const debug = require('debug')('example:server');
 
 const port = parseInt(process.env.PORT || '3000', 10);
 app.set('port', port);
 
 app.listen(app.get('port'), function () {
-  debug(`User server listening on port ${app.get('port')}`);
+  console.log(`User server listening on port ${app.get('port')}`);
 });
